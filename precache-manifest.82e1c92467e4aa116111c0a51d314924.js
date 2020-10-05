e6fb2fd49c1ecae5ed06350739db474ac0211038
aayushsaini <aayush.r98@gmail.com>
2020-10-05 20:04:38 +0530

    Build v2

---
+++ precache-manifest.82e1c92467e4aa116111c0a51d314924.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f5a8f344c8c51dc2beea08f9e8ae34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bede98c9f4c0c9330a0a",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "02b83a5f7a0bf42d0218",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bede98c9f4c0c9330a0a",
+    "url": "/static/js/2.ed6a495f.chunk.js"
+  },
+  {
+    "revision": "06617e4042528c3fcda07915cea80148",
+    "url": "/static/js/2.ed6a495f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b83a5f7a0bf42d0218",
+    "url": "/static/js/main.41070fc3.chunk.js"
+  },
+  {
+    "revision": "7581bf606506ed169535",
+    "url": "/static/js/runtime-main.63896c1b.js"
+  },
+  {
+    "revision": "daa771a7f18475c5895c7f289d7bb51b",
+    "url": "/static/media/logox.daa771a7.png"
+  },
+  {
+    "revision": "14a314466c262fd4b3dcd60e6ff0d3ab",
+    "url": "/static/media/map.14a31446.geojson"
+  }
+]);
